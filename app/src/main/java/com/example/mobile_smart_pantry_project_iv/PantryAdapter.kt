7780c5edb60bfe2f2a02281171b0fca40720a758
szymonh <szymon.haponik@cyfroweszkoly.pl>
2026-03-02 14:05:11 +0100

    [CREATE] PantryAdapter class for list view

---
+++ app/src/main/java/com/example/mobile_smart_pantry_project_iv/PantryAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.mobile_smart_pantry_project_iv
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.example.mobile_smart_pantry_project_iv.model.Product
+
+class PantryAdapter (private val context: Context,
+                    private val products: List<Product>
+    ): ArrayAdapter<Product>(context, 0, products) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        val itemView = convertView ?: LayoutInflater.from(context).inflate(
+            R.layout.item_product, parent, false
+        )
+
+        val product = products[position]
+
+        val resId = context.resources.getIdentifier(
+            product.ImageRef,
+            "drawable",
+            context.packageName
+        )
+
+
+        val nameTextView = itemView.findViewById<TextView>(R.id.productNameTextView)
+        val categoryTextView = itemView.findViewById<TextView>(R.id.productCategoryTextView)
+        val quantityTextView = itemView.findViewById<TextView>(R.id.productQuantityTextView)
+        val productImageView = itemView.findViewById<ImageView>(R.id.productImageView)
+
+        nameTextView.text = product.Name
+        categoryTextView.text = product.Category
+        quantityTextView.text = product.Quantity.toString()
+
+        if (resId !=0) productImageView.setImageResource(resId) else productImageView.setImageResource(R.drawable.ic_launcher_foreground)
+
+
+
+        return itemView
+    }
+}
