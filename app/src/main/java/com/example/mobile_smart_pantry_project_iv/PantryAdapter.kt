cbd31dd960686fe218ad4a4e7dc0497379231731
szymonh <szymon.haponik@cyfroweszkoly.pl>
2026-03-09 12:58:27 +0100

    [UPDATE] add dynamic colors for low quantity elements

--- app/src/main/java/com/example/mobile_smart_pantry_project_iv/PantryAdapter.kt
+++ app/src/main/java/com/example/mobile_smart_pantry_project_iv/PantryAdapter.kt
@@ -1,11 +1,14 @@
 package com.example.mobile_smart_pantry_project_iv
 
 import android.content.Context
+import android.graphics.Color
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContentProviderCompat.requireContext
 import com.example.mobile_smart_pantry_project_iv.model.Product
@@ -40,6 +43,14 @@ class PantryAdapter (private val context: Context,
 
         if (resId !=0) productImageView.setImageResource(resId) else productImageView.setImageResource(R.drawable.ic_launcher_foreground)
 
+        if (product.Quantity<=5){
+            Log.e("ProductData", product.Name)
+            Log.e("ProductData", product.Quantity.toString())
+            itemView.findViewById<LinearLayout>(R.id.backgroudLinearLayout).setBackgroundColor(Color.RED)
+        }
+        else{
+            itemView.findViewById<LinearLayout>(R.id.backgroudLinearLayout).setBackgroundColor(Color.MAGENTA)
+        }
 
 
         return itemView
